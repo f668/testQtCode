5316ff199f038e87a138c7302d673f4094e655dc
fengguangdong <2978158698@qq.com>
2021-04-01 21:20:45 +0800

    test第一次提交在Qt下

---
+++ mainwindow.cpp
@@ -0,0 +1,34 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QLineSeries>
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+
+    QLineSeries *series = new QLineSeries();
+    *series << QPointF(11, 1)
+            << QPointF(13, 3)
+            << QPointF(17, 6)
+            << QPointF(18, 3)
+            << QPointF(20, 2);
+
+    QChart *chart = new QChart();
+    chart->legend()->hide();
+    chart->addSeries(series);
+    chart->createDefaultAxes();
+    chart->setTitle("Simple line chart example");
+
+    ui->chartView->setChart(chart);
+    ui->chartView->setRenderHint(QPainter::Antialiasing);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ mainwindow.h
@@ -0,0 +1,26 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtCharts/QChartGlobal>
+#include <QtCharts/QChart>
+
+QT_CHARTS_USE_NAMESPACE
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
